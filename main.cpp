01ac3d79f61aa5fe939b4d2cf36902333260e9db
Leonardo-Lazzara <91598283+Leonardo-Lazzara@users.noreply.github.com>
2021-11-11 22:29:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n=0, a=1, b=1, c=0;
+   cin >> n;
+   if (n >= 2){
+      cout << a << endl;
+      cout << b << endl;
+      for (int i=3; i<=n; i++){
+         c = (a+b);
+         a=b;
+         b=c;
+         cout << c << endl;
+      }
+   }else{
+      cout << "errore" << endl;
+   }     
    return 0;
 }
